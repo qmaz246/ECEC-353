8b4935da6fd8875faad30474b8fca5a2091769c6
oto25 <52175963+oto25@users.noreply.github.com>
2020-01-25 18:22:18 -0500

    PP1_Odemis.c
    Additions of signal handlers

--- Problem1/primes.c
+++ Problem1/primes.c
@@ -1,11 +1,13 @@
  /* Skeleton code from primes.
   *
-  * Author: Naga Kandasamy
+  * Author: Naga Kandasamyuthor: Naga Kandasamy
   *
   * Date created: June 28, 2018
   * Date updated: January 16, 2020 
   *
   * Build your code as follows: gcc -o primes primes.c -std=c99 -Wall
+  * 
+  * Team Members: Quinn Mazzilli & Omer Odemis
   *
   * */
 
@@ -13,11 +15,16 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <limits.h>
+#include <signal.h>
+#include <setjmp.h>
 
-#define FALSE 0
-#define TRUE !FALSE
+#define FALSE 0 // FALSE = 0
+#define TRUE !FALSE // TRUE is the opposite of FALSE | TRUE = 1
+
+static void custom_signal_handler (int);
 
 unsigned long int num_found; /* Number of prime numbers found */
+unsigned long int last_prime[5] = {0, 0, 0, 0, 0};
 
 int 
 is_prime (unsigned int num)
@@ -44,27 +51,62 @@ is_prime (unsigned int num)
 }
 
 /* Complete the function to display the last five prime numbers found by the 
- * program, either upon normal program termination or upon being terminated 
- * via a SINGINT or SIGQUIT signal. 
- */
+ *  * program, either upon normal program termination or upon being terminated 
+ *   * via a SINGINT or SIGQUIT signal. 
+ *    */
 void 
 report ()
 {
+    for (int j = 0; j < 5; j++){
+       	fflush(stdout);
+   	printf("%lu\n", last_prime[j]);
+    }
 }
 
 int 
 main (int argc, char** argv)
 {
+    signal (SIGINT, custom_signal_handler);
+    signal (SIGQUIT, custom_signal_handler);
+//    unsigned long int last_prime[5] = {0, 0, 0, 0, 0};
     unsigned long int num;
+
     num_found = 0;
 
     printf ("Beginning search for primes between 1 and %lu. \n", LONG_MAX);
     for (num = 1; num < LONG_MAX; num++) {
         if (is_prime (num)) {
             num_found++;
-            printf ("%lu \n", num);
+	    printf ("\n");
+            printf ("%lu\n", num);
+
+	    fflush(stdout);
+	    last_prime[(num_found % 5)] = num;
+	    report();
+		
+	    sleep(1);
         }
     }
-
+    
     exit (EXIT_SUCCESS);
+}
+
+static void 
+custom_signal_handler (int signalNumber)
+{
+    switch (signalNumber){
+        case SIGINT:
+            signal (SIGINT, custom_signal_handler); /* Restablish the signal handler for the next occurrence */
+            printf ("Ouch...Caught the Control+C signal. \n");
+            break;
+
+         case SIGQUIT: 
+            printf ("Caught the Quit signal. \n");
+            printf ("Going through an orderly shutdown process. \n");
+            exit (EXIT_SUCCESS);
+
+         default: 
+            break;
+
+    }
 }
